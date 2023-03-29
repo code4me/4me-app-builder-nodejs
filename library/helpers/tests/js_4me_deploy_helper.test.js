11073c544666f2f5c8cd557c1baea70b7c0290f0
Fried Hoeben <fried.hoeben@4me.com>
2023-03-29 15:48:15 +0200

    Prevent trying to update automation rule to work on different record type

---
+++ library/helpers/tests/js_4me_deploy_helper.test.js
@@ -0,0 +1,225 @@
+'use strict';
+
+const Js4meDeployHelper = require('../js_4me_deploy_helper');
+
+describe('grouping automation rules by record type', () => {
+  test('rules with same generic value', () => {
+    const jsHelper = new Js4meDeployHelper();
+    const existingRules = [
+      {
+        "id": "abc",
+        "name": "Trigger webhook for each note added",
+        "generic": "request"
+      },
+      {
+        "id": "def",
+        "name": "Trigger webhook for status changed",
+        "generic": "request"
+      },
+    ];
+    const inputRules = [
+      {
+        "name": "Trigger webhook for each note added updated",
+        "generic": "request"
+      },
+    ];
+    const grouped = jsHelper.groupOfferingAutomationRules(existingRules, inputRules);
+    expect(Object.keys(grouped)).toEqual(['request']);
+    expect(grouped['request']).toEqual([existingRules, inputRules]);
+  });
+
+  test('rules with different generic value', () => {
+    const jsHelper = new Js4meDeployHelper();
+    const existingRules = [
+      {
+        "id": "abc",
+        "name": "Trigger webhook for each note added",
+        "generic": "request"
+      },
+      {
+        "id": "def",
+        "name": "Trigger webhook for status changed",
+        "generic": "request"
+      },
+    ];
+    const inputRules = [
+      {
+        "name": "Trigger webhook for each note added updated",
+        "generic": "task"
+      },
+    ];
+    const grouped = jsHelper.groupOfferingAutomationRules(existingRules, inputRules);
+    expect(Object.keys(grouped)).toEqual(['request', 'task']);
+    expect(grouped['request']).toEqual([existingRules, []]);
+    expect(grouped['task']).toEqual([[], inputRules]);
+  });
+
+  test('all new rules', () => {
+    const jsHelper = new Js4meDeployHelper();
+    const inputRules = [
+      {
+        "name": "Trigger webhook for each note added updated",
+        "generic": "task"
+      },
+    ];
+    const grouped = jsHelper.groupOfferingAutomationRules(null, inputRules);
+    expect(Object.keys(grouped)).toEqual(['task']);
+    expect(grouped['task']).toEqual([[], inputRules]);
+  });
+
+
+  test('remove all current rules', () => {
+    const jsHelper = new Js4meDeployHelper();
+    const existingRules = [
+      {
+        "id": "abc",
+        "name": "Trigger webhook for each note added",
+        "generic": "request"
+      },
+      {
+        "id": "def",
+        "name": "Trigger webhook for status changed",
+        "generic": "request"
+      },
+    ];
+    const grouped = jsHelper.groupOfferingAutomationRules(existingRules, null);
+    expect(Object.keys(grouped)).toEqual(['request']);
+    expect(grouped['request']).toEqual([existingRules, []]);
+  });
+
+  test('mixed rules remove one', () => {
+    const jsHelper = new Js4meDeployHelper();
+    const existingRules = [
+      {
+        "id": "abc",
+        "name": "Trigger webhook for each note added",
+        "generic": "request"
+      },
+      {
+        "id": "xyz",
+        "name": "Trigger webhook for each note added updated",
+        "generic": "request"
+      },
+      {
+        "id": "def",
+        "name": "Trigger webhook for status changed",
+        "generic": "task"
+      },
+    ];
+    const inputRules = [
+      {
+        "name": "Trigger webhook for each note added",
+        "generic": "request"
+      },
+      {
+        "name": "Trigger webhook for status changed",
+        "generic": "task"
+      },
+    ];
+    const grouped = jsHelper.groupOfferingAutomationRules(existingRules, inputRules);
+    expect(Object.keys(grouped)).toEqual(['request', 'task']);
+    expect(grouped['request']).toEqual([[existingRules[0], existingRules[1]], [inputRules[0]]]);
+    expect(grouped['task']).toEqual([[existingRules[2]], [inputRules[1]]]);
+  });
+
+  test('mixed rules add one', () => {
+    const jsHelper = new Js4meDeployHelper();
+    const existingRules = [
+      {
+        "id": "abc",
+        "name": "Trigger webhook for each note added",
+        "generic": "request"
+      },
+      {
+        "id": "def",
+        "name": "Trigger webhook for status changed",
+        "generic": "task"
+      },
+    ];
+    const inputRules = [
+      {
+        "name": "Trigger webhook for each note added",
+        "generic": "request"
+      },
+      {
+        "name": "Trigger webhook for each note added extra",
+        "generic": "request"
+      },
+      {
+        "name": "Trigger webhook for status changed",
+        "generic": "task"
+      },
+    ];
+    const grouped = jsHelper.groupOfferingAutomationRules(existingRules, inputRules);
+    expect(Object.keys(grouped)).toEqual(['request', 'task']);
+    expect(grouped['request']).toEqual([[existingRules[0]], [inputRules[0], inputRules[1]]]);
+    expect(grouped['task']).toEqual([[existingRules[1]], [inputRules[2]]]);
+  });
+
+  test('mixed rules remove all one type', () => {
+    const jsHelper = new Js4meDeployHelper();
+    const existingRules = [
+      {
+        "id": "abc",
+        "name": "Trigger webhook for each note added",
+        "generic": "request"
+      },
+      {
+        "id": "xyz",
+        "name": "Trigger webhook for each note added updated",
+        "generic": "request"
+      },
+      {
+        "id": "def",
+        "name": "Trigger webhook for status changed",
+        "generic": "task"
+      },
+    ];
+    const inputRules = [
+      {
+        "name": "Trigger webhook for status changed",
+        "generic": "task"
+      },
+    ];
+    const grouped = jsHelper.groupOfferingAutomationRules(existingRules, inputRules);
+    expect(Object.keys(grouped)).toEqual(['request', 'task']);
+    expect(grouped['request']).toEqual([[existingRules[0], existingRules[1]], []]);
+    expect(grouped['task']).toEqual([[existingRules[2]], [inputRules[0]]]);
+  });
+
+  test('non generic rules', () => {
+    const jsHelper = new Js4meDeployHelper();
+    const existingRules = [
+      {
+        "id": "abc",
+        "name": "Trigger webhook for each note added",
+      },
+      {
+        "id": "xyz",
+        "name": "Trigger webhook for each note added updated",
+      },
+    ];
+    const inputRules = [
+      {
+        "name": "Trigger webhook for status changed",
+      },
+    ];
+    const grouped = jsHelper.groupOfferingAutomationRules(existingRules, inputRules);
+    expect(Object.keys(grouped)).toEqual(['undefined']);
+    expect(grouped['undefined']).toEqual([[existingRules[0], existingRules[1]], [inputRules[0]]]);
+  });
+
+  test('no rules', () => {
+    const jsHelper = new Js4meDeployHelper();
+    const grouped = jsHelper.groupOfferingAutomationRules([], []);
+    expect(Object.keys(grouped)).toEqual([]);
+    expect(grouped).toEqual({});
+  });
+
+  test('null rules', () => {
+    const jsHelper = new Js4meDeployHelper();
+    const grouped = jsHelper.groupOfferingAutomationRules(null, null);
+    expect(Object.keys(grouped)).toEqual([]);
+    expect(grouped).toEqual({});
+  });
+});
