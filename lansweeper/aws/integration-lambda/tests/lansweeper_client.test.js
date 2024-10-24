7f545f825d9274028cb36035cbeaad03ed22cd0a
Fried Hoeben <fried.hoeben@4me.com>
2024-10-24 10:44:45 +0200

    Handle many asset types selected

--- lansweeper/aws/integration-lambda/tests/lansweeper_client.test.js
+++ lansweeper/aws/integration-lambda/tests/lansweeper_client.test.js
@@ -55,4 +55,104 @@ describe('retrieving installations with error for one site', () => {
       expect(result).toEqual([{id: "a", name: "install A for b", siteId: "b"}, {id: "b", name: "install B for b", siteId: "b"}]);
     });
   });
+
+  describe('getFilters', () => {
+    const checkFilter = async (assetTypes, expectedFilter) => {
+      const client = createClient();
+
+      const result = await client.getFilters(undefined, new Date(2024, 9, 2), "e3", assetTypes);
+      if (cleanFilter(result) !== cleanFilter(expectedFilter)) {
+        expect(result).toEqual(expectedFilter);
+      }
+    }
+
+    const cleanFilter = (query) => query.replaceAll(/\s+/g, ' ').trim()
+
+    describe('asset filtering', () => {
+      it('no asset filtered', async () => {
+        const expectedQuery = `
+          { conjunction: AND
+            groups: [ 
+              { conjunction: OR
+                conditions: [
+                  { operator: GREATER_THAN, path: "assetBasicInfo.lastSeen", value: "2024-10-01T22:00:00.000Z" },
+                  { operator: EXISTS, path: "assetBasicInfo.lastSeen", value: "false" } 
+                ]
+              },
+              { conjunction: AND
+                conditions: [
+                  { operator: EQUAL, path: "installationId", value: "e3" }
+                ]
+              }
+            ]
+          }`;
+        await checkFilter(null, expectedQuery);
+        await checkFilter(undefined, expectedQuery);
+      });
+
+      it('few assets', async () => {
+        const expectedQuery = `
+          { conjunction: AND
+            groups: [
+              { conjunction: OR
+                conditions: [ { operator: REGEXP, path: "assetBasicInfo.type", value: "Windows|Windows CE" } ]
+              },
+              { conjunction: OR
+                conditions: [
+                  { operator: GREATER_THAN, path: "assetBasicInfo.lastSeen", value: "2024-10-01T22:00:00.000Z" },
+                  { operator: EXISTS, path: "assetBasicInfo.lastSeen", value: "false" }
+                ]
+              },
+              { conjunction: AND
+                conditions: [ { operator: EQUAL, path: "installationId", value: "e3" } ]
+              }
+            ]
+          }`;
+        await checkFilter(['Windows', 'Windows CE'], expectedQuery);
+      });
+
+      it('many assets', async () => {
+        const assetTypes = [
+          "Automotive",
+          "Mobile",
+          "Monitor",
+          "NAS",
+          "Switch",
+          "Tablet",
+          "Toy",
+          "VMware Guest",
+          "Virtual Machine",
+          "Weather",
+          "Webserver",
+          "Wifi",
+          "Windows",
+        ];
+        expect(assetTypes.join('|').length).toBeGreaterThan(LansweeperClient.maxFilterFieldLength);
+
+        const expectedFilter = `
+        { conjunction: AND
+          groups: [
+            { conjunction: OR
+             conditions: [
+                { operator: REGEXP, path: "assetBasicInfo.type", value: "Automotive|Mobile|Monitor|NAS|Switch|Tablet|Toy|VMware Guest|Virtual Machine|Weather|Webserver|Wifi" },
+                { operator: REGEXP, path: "assetBasicInfo.type", value: "Windows" }
+              ]
+            },
+            { conjunction: OR
+              conditions: [
+                { operator: GREATER_THAN, path: "assetBasicInfo.lastSeen", value: "2024-10-01T22:00:00.000Z" },
+                { operator: EXISTS, path: "assetBasicInfo.lastSeen", value: "false" }
+              ]
+            },
+            { conjunction: AND
+              conditions: [
+                { operator: EQUAL, path: "installationId", value: "e3" }
+              ]
+            }
+         ]
+        }`;
+        await checkFilter(assetTypes, expectedFilter);
+      });
+    })
+  });
 });
